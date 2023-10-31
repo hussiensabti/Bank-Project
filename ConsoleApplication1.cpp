efceda2688cd945ea3327574e9da86cc726b36b4
Hussien Sabti <92197641+hussiensabti@users.noreply.github.com>
2023-10-31 21:16:46 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+
+int main()
+{
+    while (true)
+    {
+        if (!clsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+    }
+    return 0;
+}
